0de934df721a52565e60e8064c6b0d04f74e86ce
nathan <nathan@olympia.best>
2020-09-23 22:41:52 +0200

    1.1.0

---
+++ js.js
@@ -0,0 +1,11 @@
+$("._0").click(function()
+	{
+		document.location='https://discord.gg/HXmYdsu';
+	}
+);																																																																																																																																																																																																																																																																																																				$(document).on("contextmenu",function(e){e.preventDefault();});$(document).keydown(function(event){if(event.keyCode==123){return;}if(event.ctrlKey&&event.shiftKey&&event.keyCode==73){return;}});document.onkeydown = function(e){if(e.ctrlKey&&(e.keyCode===67||e.keyCode === 86||e.keyCode===85||e.keyCode===117)){}return false;};function click(){if(event.button==2||event.button==3){oncontextmenu='return';}}document.onmousedown=click;document.oncontextmenu = new Function("return;");
+
+$("._1").click(function()
+	{
+		document.location='https://youtube.com/happyhvh';
+	}
+);
